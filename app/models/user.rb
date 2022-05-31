77b815ce4defb6e00cbeed26b2b7c1f097654c83
Kaan Zenginer <kaan.zenginer@hotmail.co.uk>
2022-05-31 13:09:17 +0200

    finalises the friendship user relationship using has_many friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_one_attached :photo
+
+  has_many :friendships_as_asker, class_name: "Friendship", foreign_key: :asker_id
+  has_many :friendships_as_receiver, class_name: "Friendship", foreign_key: :receiver_id
   
+  has_one_attached :photo
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
