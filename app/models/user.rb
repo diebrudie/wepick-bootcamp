f1fc45ee1ad1360bffb14e6ec156e1116bf0a84e
Kaan Zenginer <kaan.zenginer@hotmail.co.uk>
2022-05-31 15:12:37 +0200

    creates has many and belongs to relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :proposals, dependent: :destroy
+  has_many :activities, dependent: :destroy
+  has_many :participants, dependent: :destroy
+  has_many :votes, dependent: :destroy
+
+
   has_many :friendships_as_asker, class_name: "Friendship", foreign_key: :asker_id
   has_many :friendships_as_receiver, class_name: "Friendship", foreign_key: :receiver_id
 
