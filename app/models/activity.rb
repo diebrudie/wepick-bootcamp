eceda9b9534e86575a4173331042691155913edc
Kaan Zenginer <kaan.zenginer@hotmail.co.uk>
2022-05-31 16:15:24 +0200

    activity and user validations done

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,9 @@ class Activity < ApplicationRecord
   has_many :proposals, dependent: :destroy
   has_many :participants, dependent: :destroy
   has_one_attached :photo
+
+  validates :title, presence: { strict: true }, length: { maximum: 45 }
+  validates :type, presence: { strict: true }
+  validates :activity_date, presence: { strict: true }
+  validates :deadline_date, presence: { strict: true }
 end
