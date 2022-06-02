c1dc62642ca38504b93e140b8b2747dc706045ef
Sergio Pizarro <sergio.pizarroseg@gmail.com>
2022-06-02 18:00:43 +0200

    proposals almsot ready

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
 
 
   has_many :friendships_as_asker, class_name: "Friendship", foreign_key: :asker_id
-  has_many :friendships_as_reciever, class_name: "Friendship", foreign_key: :reciever_id
+  has_many :friendships_as_receiver, class_name: "Friendship", foreign_key: :receiver_id
 
   has_one_attached :photo
 
