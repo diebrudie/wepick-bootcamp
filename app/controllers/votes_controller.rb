f91de7156c0c9327ba4ca0c8cacc3723f6b49501
isabel bruda <ibrudap@gmail.com>
2022-06-07 10:02:27 +0200

    vote controller created

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,25 @@
 class VotesController < ApplicationController
+  before_action :find_proposal, only: %i[create destroy]
 
-  def toggle
+  def create
+    @vote = Vote.new(review_params)
+    @vote.user = current_user
+    @vote.proposal = @proposal
+    @vote.save
   end
-  
+
+  def destroy
+    @vote.destroy
+    # redirect_to activities_path, status: :see_other
+  end
+
+  private
+
+  def find_proposal
+    @proposal = Proposal.find(params[:proposal_id])
+  end
+
+  def vote_params
+    params.require(:vote).permit(:user_id, :proposal_id)
+  end
 end
